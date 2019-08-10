3296c00af87679667de7e3ce2c951c35836380f7
Tyler Turnipseed <tyler@techturnip.us>
2019-08-09 22:12:08 -0400

    added endpoint to get all post objects from the database

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -1,9 +1,30 @@
 const express = require('express')
 
+// Data base operations
 const Posts = require('../data/db.js')
 
+// Router
 const router = express.Router()
 
-// Routers
+// ================================================|
+// REQUEST HANDLERS ===============================|
+// ================================================|
+
+// POSTS ENDPOINTS --------------------------------|
+// ------------------------------------------------|
+
+// GET - '/api/posts' - Returns an array of all the
+// post objects contained in the database.
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Posts.find()
+    res.status(200).json(posts)
+  } catch (err) {
+    res.status(500).json({
+      err,
+      errMessage: 'The posts information could not be retrieved.'
+    })
+  }
+})
 
 module.exports = router
