fe87d15762acea412786a7b6b4e811fed0f39670
Tyler Turnipseed <tyler@techturnip.us>
2019-08-09 22:22:45 -0400

    added endpoint to grab single post by id

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -27,4 +27,27 @@ router.get('/', async (req, res) => {
   }
 })
 
+// GET - '/api/posts/:id' - Returns the post object
+// with the specified id.
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    // .findById() will return an array with the requested
+    // post obj
+    const post = await Posts.findById(id)
+
+    // if array is not empty send status 200, else send 404
+    post.length
+      ? res.status(200).json(post)
+      : res.status(404).json({
+          message: 'The post with the specified ID does not exist.'
+        })
+  } catch (err) {
+    res.status(500).json({
+      err,
+      errMessage: 'The post information could not be retrieved.'
+    })
+  }
+})
+
 module.exports = router
